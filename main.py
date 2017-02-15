e458ff09a8cbac124c3258e7043f35e263a21f82
Brett O'Daniell <brettlod@yahoo.com>
2017-02-15 17:47:16 -0600

    first commit

--- main.py
+++ main.py
@@ -63,37 +63,24 @@ class NewPost(Handler):
         if title and post:
             a = Post(title = title, post = post)
             a.put()
-
-            self.redirect("/")
+            ID = a.key().id()
+            self.redirect("/blog/%s" % str(ID))
         else:
             error="we need both a title and a blog post!"
             self.render_newpost(title, post, error)
 
-class ViewPostHandler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
+class ViewPost(Handler):
+    def get(self, id, title="", post="", error=""):
+        postID=Post.get_by_id(int(id))
+        title=postID.title
+        post=postID.post
 
-    def render(self, template, **kw):
-        self.write(self.render(template, **kw))
+        self.render("viewpost.html", title=title, post=post, error=error)
 
-
-    def get(self, id):
-        PostID = Post.get_by_id(int(id))
-        # title = title.get_by_id(PostID)
-        # post = post.get_by_id(PostID)
-
-        if PostID == None:
-            error="we need both a title and a blog post!"
-            title="Nothing Here"
-            self.render("main.html", title=title, post=error, error=error)
-        else:
-            self.render("main.html", title=PostID, post=PostID)
-
-    # def Post.get_by_id
 
 
 app = webapp2.WSGIApplication([
-    webapp2.Route('/blog/<id:\d+>', ViewPostHandler),
+    webapp2.Route('/blog/<id:\d+>', ViewPost),
     ('/', MainPage),
     ('/newpost', NewPost)
 ], debug=True)
